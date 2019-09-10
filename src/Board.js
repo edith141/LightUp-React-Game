5da3cf6385f9d33b7625bfb6f1b74278a75cd4fc
Ishaan <ifarooq20@gmail.com>
2019-09-10 16:35:52 +0530

    Updated game logic
    Bug Fix: Game for 5x5 board wasn't 100% solvable every time.
    New logic ensures the game is 100% solvable 100% of the time.

--- src/Board.js
+++ src/Board.js
@@ -1,15 +1,63 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import Cell from "./Cell";
 import './Board.css';
 import './neonBtn.css'
 
-//OLDy WORKING 
+//NEW WITH 100% SOLVABILITY
+
+/** Game board of Lights out.
+ *
+ * Properties:
+ *
+ * - nrows: number of rows of board
+ * - ncols: number of cols of board
+ * - chanceLightStartsOn: float, chance any cell is lit at start of game
+ *
+ * State:
+ *
+ * - hasWon: boolean, true when board is all off
+ * - board: array-of-arrays of true/false
+ *
+ *    For this board:
+ *       .  .  .
+ *       O  O  .     (where . is off, and O is on)
+ *       .  .  .
+ *
+ *    This would be: [[f, f, f], [t, t, f], [f, f, f]]
+ *
+ *  This should render an HTML table of individual <Cell /> components.
+ *
+ *  This doesn't handle any clicks --- clicks are on individual cells
+ *
+ **/
+
+function flipRCellsAround(board, nrows, ncols, coord) {
+  let [i, j] = coord.split("_").map(Number);
+  // console.log(`flippin' ${coord}`);
+
+    // if this coord is actually on board, flip it
+
+  function flip (i,j) {
+    if (j >= 0 && j < ncols && i >= 0 && i < nrows) {
+      // since it a bool
+      board[i][j] = !board[i][j];
+      }
+    }
+  
+  flip(i,j);
+  flip(i, j-1);
+  flip(i, j+1);
+  flip(i-1, j);
+  flip(i+1, j);
+  
+  return board;
+}
 
 class Board extends Component {
 
   static defaultProps = {
-    noOfRows: 2,
-    noOfCols: 2,
+    nrows: 2,
+    ncols: 2,
     chanceLightStartsOn: 0.2
   };
 
@@ -17,104 +65,111 @@ class Board extends Component {
     super(props);
     // TODO: set initial state
     this.state = {
-      won: false,
-      noOfRows: this.props.noOfRows,
-      noOfCols: this.props.noOfCols,
-      board: this.createBoard(this.props.noOfRows, this.props.noOfCols)
+      hasWon: false,
+      nrows: this.props.nrows,
+      ncols: this.props.ncols,
+      board: this.createBoard(this.props.nrows, this.props.ncols)
     };
   }
 
-  /** create a board noOfRows high/noOfCols wide, each cube randomly lit or unlit */
+  /** create a board nrows high/ncols wide, each cell randomly lit or unlit */
 
-  setRcEasy = (x, y) => {
+  setRcEasy = (x,y) => {
     this.setState({
-      won: false,
-      noOfRows: 2,
-      noOfCols: 2,
-      board: this.createBoard(2, 2)
+      hasWon: false,
+      nrows: 2,
+      ncols: 2,
+      board: this.createBoard(2,2)
     });
-
+    
   }
 
-  setRcHard = (x, y) => {
+  setRcHard = (x,y) => {
     this.setState({
-      won: false,
-      noOfRows: 5,
-      noOfCols: 5,
-      board: this.createBoard(5, 5)
+      hasWon: false,
+      nrows: 5,
+      ncols: 5,
+      board: this.createBoard(5,5)
     });
-
+    
   }
 
-  createBoard(x, y) {
+  createBoard(x,y) {
     let board = [];
- //array of T/F 
-    for (let i = 0; i < x; i++) {
+    // TODO: create array-of-arrays of true/false values
+    for (let i=0; i< x; i++) {
       let row = [];
-      for (let j = 0; j < y; j++) {
-        row.push(Math.random() < this.props.chanceLightStartsOn);
+      for (let j=0; j< y; j++) {
+        row.push(false);
         console.log(Math.random() < this.props.chanceLightStartsOn);
       }
       board.push(row);
     }
-    console.log('new voard');
 
+    for (let i = 0; i < x; i++) {
+      for (let j = 0; j < y; j++) {
+        if (Math.random() < this.props.chanceLightStartsOn) {
+          board = flipRCellsAround(board, x, y, `${i}_${j}`);
+         }
+      }
+    }
+    console.log('new voard');
     return board
   }
 
   /** handle changing a cell: update board & determine if winner */
 
-  flipCubes(coord) {
-    let noOfCols = this.state.noOfCols;
-    let noOfRows = this.state.noOfRows;
+  flipCellsAround(coord) {
+    let ncols = this.state.ncols;
+    let nrows = this.state.nrows;
     let board = this.state.board;
     let [i, j] = coord.split("_").map(Number);
     // console.log(`flippin' ${coord}`);
 
-    // if the coordinate is on the board, toggle it!
+      // if this coord is actually on board, flip it
 
-    function flip(i, j) {
-      if (j >= 0 && j < noOfCols && i >= 0 && i < noOfRows) {
+    function flip (i,j) {
+      if (j >= 0 && j < ncols && i >= 0 && i < nrows) {
         // since it a bool
         board[i][j] = !board[i][j];
+        }
       }
-    }
+    
+    flip(i,j);
+    flip(i, j-1);
+    flip(i, j+1);
+    flip(i-1, j);
+    flip(i+1, j);
+      console.log('wtf!');
 
-    flip(i, j);
-    flip(i, j - 1);
-    flip(i, j + 1);
-    flip(i - 1, j);
-    flip(i + 1, j);
-    console.log('wtf!');
-
-    let won = board.every(row => row.every(col => !col));
-    console.log(won);
-    this.setState({ board: board, won: won });
+    let hasWon = board.every(row =>  row.every( col => !col));
+    console.log(hasWon);
+    this.setState({board: board, hasWon: hasWon});
   }
 
 
   render() {
 
-    if (this.state.won) {
+    if (this.state.hasWon) {
       return (
-        <h3 className="Winner">YOU WIN!</h3>
+        <h3 className = "Winner">WINNER!</h3>
       );
     }
 
     let htmlBoard = [];
 
-    for (let i = 0; i < this.state.noOfRows; i++) {
+    for (let i=0; i<this.state.nrows; i++) {
       let row = [];
-      for (let j = 0; j < this.state.noOfCols; j++) {
+      for( let j=0; j< this.state.ncols; j++) {
         let crd = `${i}_${j}`;
-        // console.log(`${i}_${j}`)
-        row.push(<Cell key={crd} isLit={this.state.board[i][j]}
-          flipCubes={() => this.flipCubes(crd)} />);
+        console.log(`${i}_${j}`)
+        row.push(<Cell key={crd} isLit={this.state.board[i][j]} 
+        flipCellsAround = {() => this.flipCellsAround(crd)}/>);
       }
-      htmlBoard.push(<tr key={i}>{row}</tr>);
+      htmlBoard.push(<tr key = {i}>{row}</tr>);
     }
 
-    return (
+    return(
       <div>
         <div>
           <table className='Board'>
@@ -122,20 +177,18 @@ class Board extends Component {
               {htmlBoard}
             </tbody>
           </table>
-          <button className="neonBtn-easy" onClick={this.setRcEasy}> Easy </button>
-          <button className="neonBtn-hrd" onClick={this.setRcHard}> Hard </button>
+          <button className = "neonBtn-easy" onClick={this.setRcEasy}> Easy </button>
+          <button className = "neonBtn-hrd" onClick={this.setRcHard}> Hard </button>
           <div>
-            <h5 className="Rules">
-              Rules:<br></br>
-              Light up all the cubes!<br></br>
-              Clicking a cube toggles it and the adjacent neighbours.</h5>
-          </div>
+          <h5 className = "Rules">Rules:<br></br>Light up all the cubes!<br></br>Clicking a cube toggles it and the adjacent neighbours.</h5>
         </div>
+        </div>
+        
       </div>
     );
+  
   }
 }
 
 
 export default Board;
-
